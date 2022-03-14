e2bc85a5e4093b355b6c4310019ce83fa44fac3f
Ankitha A Jain <96869681+ankithaajain@users.noreply.github.com>
2022-03-14 16:20:27 +0530

    commiting changes

---
+++ Complaint Redressal System/AbcTelecomApplicationTests.java
@@ -0,0 +1,11 @@
+package com.jain.ankitha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcTelecomApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
